8a29d12b053215f324841aa1b75f084a41d0289e
DebashisDhali <dhalideba387@gmail.com>
2026-01-26 17:34:20 +0600

    Fix undefined routes in tourRoutes.js

--- backend/src/routes/tourRoutes.js
+++ backend/src/routes/tourRoutes.js
@@ -5,8 +5,7 @@ const tourController = require('../controllers/tourController');
 router.post('/', tourController.createTour);
 router.get('/', tourController.getAllTours);
 router.post('/join', tourController.joinTour);
-router.get('/:tourId/requests', tourController.getPendingRequests);
-router.post('/requests/:requestId/approve', tourController.approveJoinRequest);
+
 router.get('/:id', tourController.getTourDetails);
 
 
